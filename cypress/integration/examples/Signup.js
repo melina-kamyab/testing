237d4873dcf901f12e59de403b80b5c381cf1e70
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-27 14:53:18 +0100

    create develop and merge feturebranch

--- cypress/integration/examples/Signup.js
+++ cypress/integration/examples/Signup.js
@@ -12,44 +12,44 @@ describe("Signup form", () => {
     cy.contains("Felformatterad e-postadress").end();
   });
 
-    it("Register password incorrectly", () => {
-      cy.visit("/");
-      cy.get("a").click();
-      cy.url().should("include", "/register");
+  it("Register password incorrectly", () => {
+    cy.visit("/");
+    cy.get("a").click();
+    cy.url().should("include", "/register");
 
-      cy.get("form").should("have.id", "registerForm");
-      cy.get("input[name=username]").type("CoolUser");
-      cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
-      cy.get("input[name=password]").type("ilovesweatshirts");
-      cy.get("input[name=passwordConfirm]").type("ilovesweatshorts");
-      cy.get("form").submit();
+    cy.get("form").should("have.id", "registerForm");
+    cy.get("input[name=username]").type("CoolUser");
+    cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
+    cy.get("input[name=password]").type("ilovesweatshirts");
+    cy.get("input[name=passwordConfirm]").type("ilovesweatshorts");
+    cy.get("form").submit();
 
-      cy.contains("Lösenorden överensstämmer inte!");
+    cy.contains("Lösenorden överensstämmer inte!");
 
-      cy.get("input[name=username]").type("CoolUser");
-      cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
-      cy.get("input[name=password]").type("ilovesweatshirts");
-      cy.get("input[name=passwordConfirm]").type("Ilovesweatshorts");
-      cy.get("form").submit();
+    cy.get("input[name=username]").type("CoolUser");
+    cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
+    cy.get("input[name=password]").type("ilovesweatshirts");
+    cy.get("input[name=passwordConfirm]").type("Ilovesweatshorts");
+    cy.get("form").submit();
 
-      cy.contains("Lösenorden överensstämmer inte!");
-    });
+    cy.contains("Lösenorden överensstämmer inte!");
+  });
 
-    it("Email registration successful", () => {
-      cy.visit("/");
-      cy.get("a").click();
-      cy.url().should("include", "/register");
+  it("Email registration successful", () => {
+    cy.visit("/");
+    cy.get("a").click();
+    cy.url().should("include", "/register");
 
-      cy.get("form").should("have.id", "registerForm");
-      cy.get("input[name=username]").type("CoolUser");
-      cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
-      cy.get("input[name=password]").type("ilovesweatshirts");
-      cy.get("input[name=passwordConfirm]").type("ilovesweatshirts");
-      cy.get("form").submit();
+    cy.get("form").should("have.id", "registerForm");
+    cy.get("input[name=username]").type("CoolUser");
+    cy.get("input[name=email]").type("Frontenddudedettes@gmail.com");
+    cy.get("input[name=password]").type("ilovesweatshirts");
+    cy.get("input[name=passwordConfirm]").type("ilovesweatshirts");
+    cy.get("form").submit();
 
-      cy.url().should("include", "/register"); //KOLLA VAD SOM UTMÄRKER DENNA SIDA FÖR JAG KAN INTE HITTA DET
+    cy.url().should("include", "/register"); //KOLLA VAD SOM UTMÄRKER DENNA SIDA FÖR JAG KAN INTE HITTA DET
 
-      cy.get("a").click();
-      cy.url().should("include", "/");
-    });
+    cy.get("a").click();
+    cy.url().should("include", "/");
+  });
 });
