0aa0ad50806bf7eb27a02ccaa8f1b97a849e0420
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-27 13:15:51 +0100

    check email validity

--- cypress/integration/examples/Signup.js
+++ cypress/integration/examples/Signup.js
@@ -7,5 +7,10 @@ describe("Signup form", () => {
     cy.url().should("include", "/register");
 
     cy.get("form").should("have.id", "registerForm");
+    cy.get("input[name=username]").type("CoolUser");
+    cy.get("input[name=email]").type("email.com");
+    cy.get("form").submit();
+
+    cy.contains("Felformatterad e-postadress");
   });
 });
