fb6a0c954296fddb9501e2bc68a50a82058516a6
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-30 02:34:11 +0100

    final touch

--- cypress/integration/examples/Guestbook.js
+++ cypress/integration/examples/Guestbook.js
@@ -1,30 +1,26 @@
-describe("guestbook", () =>{
+describe("guestbook", () => {
+  it("Make guestbook post", () => {
+    cy.visit("/guestbook");
+    cy.get("form");
+    cy.get("textarea").type("Fin sida!").should("have.value", "Fin sida!");
+    cy.get("form").submit();
+    cy.contains("Fin sida!").end();
+  });
 
-    it("Make guestbook post", () =>{
-        
-        cy.visit("/guestbook")
-        cy.get("form");
-        cy.get('textarea').type("Fin sida!").should("have.value", "Fin sida!");
-        cy.get("form").submit();
-        cy.contains("Fin sida!").end();
-    })
+  it("Fail making guestbook post", () => {
+    cy.visit("/guestbook");
+    cy.get("form");
+    cy.get("textarea").type("yo");
+    cy.get("form").submit();
+    cy.contains("För kort meddelande!").end();
+  });
 
-    it("Fail making guestbook post", () =>{
-        cy.visit("/guestbook")
-        cy.get("form");
-        cy.get('textarea').type("yo");
-        cy.get("form").submit();
-        cy.contains("För kort meddelande!").end();
-    })
-
-    it("Make and delete guestbook post", () =>{
-        
-        cy.visit("/guestbook")
-        cy.get("form");
-        cy.get('textarea').type("Ful sida!").should("have.value", "Ful sida!");
-        cy.get("form").submit();
-        cy.get("p").contains("Ful sida!").contains("a").click();
-        cy.get("#entries").contains("Ful sida!").should("not.exist").end();
-
-    })
-})
+  it("Make and delete guestbook post", () => {
+    cy.visit("/guestbook");
+    cy.get("form");
+    cy.get("textarea").type("Ful sida!").should("have.value", "Ful sida!");
+    cy.get("form").submit();
+    cy.get("p").contains("Ful sida!").contains("a").click();
+    cy.get("#entries").contains("Ful sida!").should("not.exist").end();
+  });
+});
