a766730c381a4c4237430a8ac324d32d4e18f0e7
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-30 02:37:30 +0100

    removed a comment

--- cypress/integration/examples/Login.js
+++ cypress/integration/examples/Login.js
@@ -1,25 +1,24 @@
-//Evelina
+describe("Login form", () => {
+  it("Can sign in", () => {
+    cy.visit("/");
+    cy.get("form");
+    cy.get('input[name="username"]')
+      .type("CoolUser")
+      .should("have.value", "CoolUser");
+    cy.get('input[name="password"]')
+      .type("123123123")
+      .should("have.value", "123123123");
+    cy.get("form").submit();
+    cy.url().should("include", "start");
+    cy.contains("Välkommen CoolUser").end();
+  });
 
-
-describe("Login form", () =>{
-
-    it("Can sign in", () =>{
-        cy.visit("/");
-        cy.get("form");
-        cy.get('input[name="username"]').type("CoolUser").should("have.value", "CoolUser");
-        cy.get('input[name="password"]').type("123123123").should("have.value", "123123123");
-        cy.get("form").submit();
-        cy.url().should("include", "start");
-        cy.contains("Välkommen CoolUser").end();
-    })
-
-    it("Cant sign in", () =>{
-        cy.visit("/");
-        cy.get("form");
-        cy.get('input[name="username"]').type("CoolUser");
-        cy.get('input[name="password"]').type("asd");
-        cy.get("form").submit();
-        cy.url().should("include","wronglogin").end();
-
-    })
-}) 
+  it("Cant sign in", () => {
+    cy.visit("/");
+    cy.get("form");
+    cy.get('input[name="username"]').type("CoolUser");
+    cy.get('input[name="password"]').type("asd");
+    cy.get("form").submit();
+    cy.url().should("include", "wronglogin").end();
+  });
+});
